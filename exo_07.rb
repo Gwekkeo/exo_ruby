1d7cd1a629960916838b9732a5344ed219aea6a4
Gwen <gwendoline.cassand@gmail.com>
2018-06-10 18:41:10 +0200

    THP ajout de l'exo 7 + explication des différences entre les 3 programmes proposé

---
+++ exo_07.rb
@@ -0,0 +1,8 @@
+user_name = gets.chomp
+puts user_name
+
+# chomp demande à l'utilisateur d'entrer quelque chose
+#Le premier programme demande d'entrer son nom puis affiche ce qu'a rentrer l'utilisateur
+#le 2eme programme demande d'entrer son nom puis affiche un > pour que l'utilisateur voit où il va ecrire
+#le 3eme n'affiche pas de texte, donc l'utilisateur ne sait pas ce qu'il doit ecrire et quand ecrire
+
